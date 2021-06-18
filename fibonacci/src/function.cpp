759b37d287f41f7cf68cf789e2e3d0b08c3fb95f
João Filipe Silva <joaofilipenasc@gmail.com>
2021-06-17 21:27:02 -0300

    Att

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,31 @@
 #include "function.h"
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
+using namespace std;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
+unsigned int fib(unsigned int num)
+{
+    if (num == 0 || num == 1)
+    {
+        return num;
+    }
+    else
+    {
+        return (fib(num - 1) + fib(num - 2));
+    }
+}
+
+std::vector<unsigned int> fib_below_n(unsigned int n) 
+{
+    
+    int n = 0;
+    int result = 0;
+
+    cin >> n;
+
+    for (int i = 0; (result = fib(i)) < k; i++) 
+    { 
+        cout << result << endl; 
+    }               
+
     return std::vector<unsigned int>{};
 }
