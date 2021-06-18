952613b3f385ca698a9f3b695cb96443731c8253
João Filipe Silva <joaofilipenasc@gmail.com>
2021-06-18 12:55:52 -0300

    Att

--- filter/src/function.cpp
+++ filter/src/function.cpp
@@ -1,21 +1,73 @@
-/*! 
- * Remove negative and zero values from an array, preserving the
- * relative order of the original values that will remain in the
- * resulting array.
- * We do not alter the origimal memory associated withe the input
- * array. Rather, we just rearrange the values inside the array
- * and return a pointer to the new 'logical' end of the array,
- * after the processes of eliminating the required elements is
- * finished.
- *
- * @param first Pointer to the beginning of the range we want to filter.
- * @param last Pointer just past the last valid value of the range we want to filter.
- * @return a pointer to the new 'logical' end of the array.
- */
-int * filter( int * first, int * last )
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+#include "function.h"
+
+int *filtro (int * first, int * last)
 {
-    // TODO: coloque aqui o seu código-solução.
-    
-    // TODO: substitua o retorno de acordo com a solução. Isso é somente um STUB temporário.
+    while(first != last) 
+    {
+        if(first <= 0)
+        {
+            auto i(first);
+            while(i != last-1)
+            {
+                *i = *(i+1);
+                ++i;
+            }
+            last--;
+        }
+        else
+        {
+            first++;
+        }
+    }
+    return first;
     return nullptr;
+}
+
+int *mega_filtro (int * first, int * last)
+{
+    auto slow(first);
+    auto fast(first);
+
+    while(fast != last)
+    {
+        if(*fast > 0)
+        {
+          *slow = *fast;
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
+}
+
+int main () 
+{
+    int tamanho;
+    int array[tamanho];
+    
+    for (int i = 0; i < tamanho; i++)
+    {
+        cin >> array[i];
+    }
+
+    std::cout << "Array original = [ ";
+    for (int i = 0; i < tamanho; ++i)
+    {
+        cout << array[i];
+    }
+    std::cout << " ]" <<  std::endl;
+
+    auto new_end = mega_filtro(begin(array), end(array));
+
+    std::cout << ">>> Array filtrado  = [ ";
+    std::copy( std::begin(array), new_end,  std::ostream_iterator<int>( std::cout, " " ) );
+    std::cout << " ]" <<  std::distance(std::begin(A), new_end) << "\n";
+
+    return EXIT_SUCCESS;
 }
